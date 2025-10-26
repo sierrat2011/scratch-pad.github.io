9ef851147821effef0a947b0e54efef5cbe296e5
sierrat2011 <sierratrim@gmail.com>
2025-10-26 12:02:26 -0500

    added comments to getObjectValues and fizzBuzz

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -15,8 +15,8 @@ function fizzBuzz() {
  for (var i = 0; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz")
-  } else if (i % 3 === 0) {
-    console.log("Fizz")
+  } else if (i % 3 === 0) {             // I think this is right
+    console.log("Fizz")                // it worked in codepen 
   } else if (i % 5 === 0) {
     console.log("Buzz")
   } else {
